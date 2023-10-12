e626af3ba61c16db367730828727fa8c3fb41798
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-12 17:38:22 +0500

    счетаем ожидаемую зарплату

--- hh_api.py
+++ hh_api.py
@@ -1,24 +1,33 @@
 import requests
 
-languages = ["Python", "Java", "Javascript", "Go", "PHP", "C++", "TypeScript", "C#", "Shell"]
-vacancies_count = {}
-vacancies_salaries = {}
 
-for language in languages:
-    url = "https://api.hh.ru/vacancies?text=программист {}&area=1".format(language)
+def predict_rub_salary(vacancy):
+    url = "https://api.hh.ru/vacancies?text={}&area=1".format(vacancy)
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-        vacancies_count[language] = data['found']
-        vacancies_salaries[language] = []
         vacancies = data['items']
-        for vacancy in vacancies:
-            if vacancy['salary'] is not None:
-                vacancies_salaries[language].append(vacancy['salary'])
-    else:
-        print("Ошибка при выполнении запроса:", response.status_code)
 
-print("Количество вакансий:", vacancies_count)
-print("Вакансии Зарплаты:", vacancies_salaries)
+        expected_salaries = []
+        for vacancy in vacancies:
+            salary = vacancy['salary']
+            if salary and salary['currency'] == 'RUR' and salary['gross'] is False:
+                if salary['from'] and salary['to']:
+                    expected_salaries.append((salary['from'] + salary['to']) / 2)
+                elif salary['from']:
+                    expected_salaries.append(salary['from'] * 1.2)
+                elif salary['to']:
+                    expected_salaries.append(salary['to'] * 0.8)
+
+        return expected_salaries
+    else:
+        print("Ошибка выполнения запроса:", response.status_code)
+
+
+expected_salaries = predict_rub_salary('Python')
+
+print("Ожидаемые зарплаты:", len(expected_salaries))
+print(expected_salaries[:20])
+
 
