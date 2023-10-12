f91cd8595ebb3ed5776be3ba4ba4313a2b44ca19
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-12 17:26:34 +0500

    добавил поиск зарплаты

--- hh_api.py
+++ hh_api.py
@@ -2,6 +2,7 @@ import requests
 
 languages = ["Python", "Java", "Javascript", "Go", "PHP", "C++", "TypeScript", "C#", "Shell"]
 vacancies_count = {}
+vacancies_salaries = {}
 
 for language in languages:
     url = "https://api.hh.ru/vacancies?text=программист {}&area=1".format(language)
@@ -10,8 +11,14 @@ for language in languages:
     if response.status_code == 200:
         data = response.json()
         vacancies_count[language] = data['found']
+        vacancies_salaries[language] = []
+        vacancies = data['items']
+        for vacancy in vacancies:
+            if vacancy['salary'] is not None:
+                vacancies_salaries[language].append(vacancy['salary'])
     else:
         print("Ошибка при выполнении запроса:", response.status_code)
 
-print(vacancies_count)
+print("Количество вакансий:", vacancies_count)
+print("Вакансии Зарплаты:", vacancies_salaries)
 
