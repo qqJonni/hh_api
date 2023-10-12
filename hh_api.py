8d0cb77b1c9b303eb7f0efdf36768cc98e78affb
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-12 16:56:16 +0500

    достал программистов

--- hh_api.py
+++ hh_api.py
@@ -1,8 +1,14 @@
 import requests
 
-headers = {"User-Agent": "api-test-agent"}
-params = {"areas.name": "Москва"}
-response = requests.get(url='https://api.hh.ru/vacancies',params=params, headers=headers)
+url = "https://api.hh.ru/vacancies?text=программист"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+
+else:
+    print("Ошибка при выполнении запроса:", response.status_code)
 
 print(response.text)
 
