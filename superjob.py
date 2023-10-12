a8886448b5d27e63168cbc2d98ab21b29bae9499
Valeriy Lvov <tepliy_shov@mail.ru>
2023-10-12 19:48:32 +0500

    сделана выборка по профессии и городу

--- superjob.py
+++ superjob.py
@@ -5,17 +5,21 @@ from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
 headers = {
-    'X-Api-App-Id': os.environ.get('SUPERJOB_SECRET_KEY')}
+    'X-Api-App-Id': os.environ.get('SUPERJOB_SECRET_KEY')
+}
 
 url = "https://api.superjob.ru/2.0/vacancies/"
 
-response = requests.get(url, headers=headers)
+params = {
+    'catalogues': 48,
+    'town': 4,
+}
+
+response = requests.get(url, headers=headers, params=params)
 
 if response.status_code == 200:
     data = response.json()
-
     for vacancy in data['objects']:
         print(vacancy['profession'])
-
 else:
     print("Ошибка при выполнении запроса:", response.status_code)
