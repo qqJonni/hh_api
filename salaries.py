2dc1be991168050de684dbb421628503acd2acc6
Valeriy Lvov <tepliy_shov@mail.ru>
2023-11-10 20:18:09 +0400

    связал числа с кодом

--- salaries.py
+++ salaries.py
@@ -4,6 +4,11 @@ from dotenv import load_dotenv, find_dotenv
 from statistics import mean
 from terminaltables import AsciiTable
 
+AREA_MOSCOW_HH = 1
+CATALOGUE_PROGRAMMING_SJ = 48
+TOWN_MOSCOW_SJ = 4
+
+
 def get_hh_vacancy_salaries(language):
     base_url = "https://api.hh.ru/vacancies"
     vacancies_found = 0
@@ -13,7 +18,7 @@ def get_hh_vacancy_salaries(language):
     while True:
         params = {
             'text': f"программист {language}",
-            'area': 1,
+            'area': AREA_MOSCOW_HH,
             'page': page
         }
         response = requests.get(base_url, params=params)
@@ -42,6 +47,7 @@ def get_hh_vacancy_salaries(language):
         "average_salary": average_salary
     }
 
+
 def get_sj_vacancy_salaries(language, sj_secret_key):
     base_url = "https://api.superjob.ru/2.0/vacancies/"
     vacancies_found_sj = 0
@@ -50,8 +56,8 @@ def get_sj_vacancy_salaries(language, sj_secret_key):
     page = 0
     while True:
         params = {
-            'catalogues': 48,
-            'town': 4,
+            'catalogues': CATALOGUE_PROGRAMMING_SJ,
+            'town': TOWN_MOSCOW_SJ,
             'keyword': f"программист {language}",
             'page': page
         }
@@ -78,6 +84,7 @@ def get_sj_vacancy_salaries(language, sj_secret_key):
         "vacancies_processed": len(salaries_sj),
         "average_salary": average_salary_sj
     }
+
 
 def main():
     languages = ["Python", "Java", "Javascript", "Go", "PHP", "C++", "TypeScript", "C#", "Shell"]
@@ -106,6 +113,7 @@ def main():
         table_data_sj.append((lang, data['vacancies_found'], data['vacancies_processed'], data['average_salary']))
     table_sj = AsciiTable(table_data_sj, "+SuperJob Moscow--------")
     print(table_sj.table)
+
 
 if __name__ == "__main__":
     load_dotenv(find_dotenv())
